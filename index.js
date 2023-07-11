3be937784201a2a21afb5162ea09bd185469dca4
Nikhil Taneja <taneja.nikhil03@gmail.com>
2023-07-11 12:23:12 +0530

    Progress bar using ascii
    progress bars made of Unicode symbols. Ex: ▰▰▰▰▰▰▰▰▰▱▱▱▱▱▱▱▱▱▱ 52%

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 require('dotenv').config()
 const { ThreadsAPI } = require('threads-api')
-const { getDayOfYear } = require('./chart')
 
 const TOTAL_DAYS_IN_A_YEAR = 365
+
+/**
+ *
+ * @param {Date} date from when you want to calculate
+ * @returns {number} number of days elapsed for that year
+ */
+const getDayOfYear = (date) => {
+  const start = new Date(date.getFullYear(), 0, 0)
+  const diff = date - start
+  const oneDay = 1000 * 60 * 60 * 24 // milliseconds in a day
+  const dayOfYear = Math.floor(diff / oneDay)
+  return dayOfYear
+}
+
+/**
+ * 
+ * @param {number} progress percentage completed
+ * @param {number} total maximum possible value
+ * @param {number} width length of progress bar
+ * @returns {string} progress bars made of Unicode symbols. Ex: ▰▰▰▰▰▰▰▰▰▱▱▱▱▱▱▱▱▱▱ 52%
+ */
+function createProgressBar(progress, total, width) {
+  const filledWidth = Math.round((progress / total) * width);
+  const emptyWidth = width - filledWidth;
+  const filledBar = '▰'.repeat(filledWidth);
+  const emptyBar = '▱'.repeat(emptyWidth);
+
+  return `${filledBar}${emptyBar}`;
+}
 
 /**
  *
@@ -29,9 +57,13 @@ const createThreadMessage = async (text, image = undefined) => {
 const today = new Date()
 const dayOfYear = getDayOfYear(today)
 const percentage = (dayOfYear / TOTAL_DAYS_IN_A_YEAR) * 100
+const progressBarWidth = 30;
+const totalProgress = 100;
+const progressBarAscii = createProgressBar(percentage, totalProgress, progressBarWidth)
 
-const message = `${today.getFullYear()} is ${percentage.toFixed(2)}% complete`
-const chartUrl = "https://github.com/itsnikhil/threads-year-in-progress/raw/main/.github/cover.jpg"
+const message = `${dayOfYear} days passed in ${today.getFullYear()}
+${progressBarAscii}
+${percentage.toFixed(2)}% complete`
 
-console.debug({ message, chartUrl })
-createThreadMessage(message, chartUrl)
+console.debug({ message })
+createThreadMessage(message)
